ffda78f98d4b7962311d15ac797f4edc2a989d22
Florent <36513774+florent-uzio@users.noreply.github.com>
2022-10-21 16:45:40 -0400

    fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next';
 
-const Home: NextPage = () => {
+export const Home: NextPage = () => {
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
-
-export default Home;
